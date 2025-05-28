0515db734c70fcd10873a98e9c1f2774677733ba
Sachin Sen <sachinsen1920@gmail.com>
2025-05-28 12:58:32 +0530

    fixed lag issue

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,12 +5,12 @@ import Hero from "../components/Hero.jsx";
 import TourCard from "../components/tourCard.jsx";
 import BookingForm from "../components/BookingForm.jsx";
 import Footer from "../components/Footer.jsx";
-import delhi from "../assets/delhi.jpg";
-import agra from "../assets/agra.jpg";
-import jaipur from "../assets/jaipur.jpg";
-import hyderabad from "../assets/hyederabad.jpg";
-import mumbai from "../assets/mumba.jpg";
-import chennai from "../assets/chennai.jpg";
+// import delhi from "../assets/delhi.jpg";
+// import agra from "../assets/agra.jpg";
+// import jaipur from "../assets/jaipur.jpg";
+// import hyderabad from "../assets/hyederabad.jpg";
+// import mumbai from "../assets/mumba.jpg";
+// import chennai from "../assets/chennai.jpg";
 import Gallery from "../components/Gallery.jsx";
 import Services from "../components/Services.jsx";
 import Packages from "../components/Packages.jsx";
@@ -23,33 +23,39 @@ const headingVariants = {
 const Home = () => {
   const destinations = [
     {
-      image: jaipur,
+      image:
+        "https://images.unsplash.com/photo-1602643163983-ed0babc39797?q=80&w=3165&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Jaipur",
       description: "Explore the Pink City with its vibrant palaces and forts.",
     },
     {
-      image: delhi,
+      image:
+        "https://plus.unsplash.com/premium_photo-1661919589683-f11880119fb7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Delhi",
       description: "Discover the historic and modern charm of India’s capital.",
     },
     {
-      image: agra,
+      image:
+        "https://images.unsplash.com/photo-1564507592333-c60657eea523?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Agra",
       description: "Visit the iconic Taj Mahal and Agra Fort.",
     },
     {
-      image: hyderabad,
+      image:
+        "https://images.unsplash.com/photo-1551161242-b5af797b7233?q=80&w=2902&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Hyderabad",
       description: "Experience the cultural richness of Hyderabad.",
     },
     {
-      image: mumbai,
+      image:
+        "https://images.unsplash.com/photo-1660145416818-b9a2b1a1f193?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Mumbai",
       description:
         "Discover the bustling city of Mumbai with its iconic landmarks.",
     },
     {
-      image: chennai,
+      image:
+        "https://images.unsplash.com/photo-1526572690437-c3f99d109cfd?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Chennai",
       description: "Immerse yourself in the cultural heritage of Chennai.",
     },
