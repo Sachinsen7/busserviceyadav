5b031f2b4aab635a58e964dcef7c12108c73bca7
Sachin Sen <sachinsen1920@gmail.com>
2025-05-29 18:04:48 +0530

    add: include additional routes for CabService, BusService, HotelService, and BookingInquiryForm

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,11 @@ import About from "./components/About.jsx";
 import Layout from "./Layout.jsx";
 import Privacy from "./Pages/Privacy.jsx";
 import NotFound from "./Pages/Notfound.jsx";
+import TourDetails from "./components/TourDetails.jsx";
+import CabService from "./Pages/CabService.jsx";
+import BusService from "./Pages/BusService.jsx";
+import HotelService from "./Pages/HotelService.jsx";
+import BookingInquiryForm from "./components/BookingInquiryForm.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -28,6 +33,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
           <Route path="/privacy" element={<Privacy />} />
+          <Route path="/cab-service" element={<CabService />} />
+          <Route path="/bus-service" element={<BusService />} />
+          <Route path="/hotel-service" element={<HotelService />} />
+          <Route path="/hotel-service" element={<BookingInquiryForm />} />
+
+          <Route path="/tour-details/:packageTitle" element={<TourDetails />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
